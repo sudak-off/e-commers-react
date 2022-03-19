7b56236ae42d6785d4986a0ff1fb1f3552b87943
Tema <krestonos@gmail.com>
2022-03-19 19:50:43 +0200

    add test function,

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 import styles from './Card.module.scss'
 
-const Card = ({ id, title, imageUrl, price, onFavorite, onPlus, favorited = false}) => {
+const Card = ({id, title, imageUrl, price, onFavorite, onPlus, favorited = false}) => {
     const [isAdded, setIsAdded] = useState(false);
     const [isFavorite, setIsFavorite] = useState(favorited);
 
@@ -16,18 +16,22 @@ const Card = ({ id, title, imageUrl, price, onFavorite, onPlus, favorited = fals
         setIsFavorite(!isFavorite)
     }
 
+    const numbFnt = (num) => {
+        return num.toLocaleString('ua-UA');
+    }
+
 
     return (
         <div className={styles.card}>
             <div className={styles.favorite} onClick={onClickFavorite}>
-                <img src={ !isFavorite ? "/img/heart-unliked.svg" : "/img/heart-like.svg" } alt="like"/>
+                <img src={!isFavorite ? "/img/heart-unliked.svg" : "/img/heart-like.svg"} alt="like"/>
             </div>
             <img width={133} height={112} src={imageUrl} alt="shoes"/>
             <h5>{title}</h5>
             <div className='d-flex justify-between align-center'>
                 <div className='d-flex flex-column'>
                     <span>Цена:</span>
-                    <b>{price} грн.</b>
+                    <b>{numbFnt(price)} грн.</b>
                 </div>
                 <button>
                     <img className={styles.plus} onClick={onClickPlus}
